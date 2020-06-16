42f1eee18f35fe59904a389e4920e03c7750bfeb
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-16 16:28:24 -0300

    Add root directory path helper.

--- routes/not-found.js
+++ routes/not-found.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '../views/not-found.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views/not-found.html'));
 });
 
 module.exports = router;
