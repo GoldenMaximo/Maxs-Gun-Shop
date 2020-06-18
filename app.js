b0731c41d751743fcd3ffd4c7ff283a6df8c37f8
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-18 15:36:50 -0300

    Fix routes and CSS links.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const rootDir = require('./util/path');
 
 const app = express();
 
@@ -10,7 +9,7 @@ const shopRoutes = require('./routes/shop');
 const notFoundRoute = require('./routes/not-found');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
