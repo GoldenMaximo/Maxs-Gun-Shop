119c25d8fa02488bc776f9f36ecf7895cf2db30f
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-22 10:57:02 -0300

    Share request data across files.

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoute = require('./routes/not-found');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use(notFoundRoute);
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views/add-product.html'));
@@ -11,8 +13,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
