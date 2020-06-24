38b756d1cf024fedb8eb98cd055da98df53e17ab
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-24 19:23:14 -0300

    Refactor Product Model.

--- models/Product.js
+++ models/Product.js
@@ -3,30 +3,30 @@ const path = require('path');
 const rootDir = require('../util/path');
 const p = path.join(rootDir, './data/products.json');
 
+const getProductsFromFile = (callback) => {
+    fs.readFile(p, (err, content) => {
+        if (err) {
+            return callback([]);
+        }
+        callback(JSON.parse(content));
+    });
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
-            });
+            })
         });
     }
 
     static fetchAll(cb) {
-        fs.readFile(p, (err, content) => {
-            if (err) {
-                return cb([]);
-            }
-            return cb(JSON.parse(content));
-        });
+        getProductsFromFile(cb);
     }
 }
