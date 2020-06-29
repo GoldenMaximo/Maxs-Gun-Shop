92b010d2c2e2f4f66389805720410f85ad899e5b
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-29 19:36:09 -0300

    Add cart model. Save cart data.

---
+++ models/cart.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const { totalmem } = require('os');
+const p = path.join(
+    path.dirname(require.main.filename),
+    './data/cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {
+                products: [],
+                totalPrice: 0
+            }
+
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+
+            const existingProductIndex = cart.products.findIndex(p => p.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty += 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = {
+                    id,
+                    qty: 1
+                }
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            })
+        })
+    }
+}
