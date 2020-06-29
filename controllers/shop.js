5460e926c5085d68a7081d6333bdd1b9ae5b3a9c
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-29 16:14:25 -0300

    Add add-to-cart template and functionality.

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,6 +41,12 @@ exports.getCart = (req, res, next) => {
     });
 };
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+}
+
 exports.getOrders = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/orders', {
