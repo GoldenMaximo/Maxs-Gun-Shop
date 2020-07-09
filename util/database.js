04868d30077e87ac794f0f4b46f0b9bd91a351f8
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-09 14:18:59 -0300

    Connect to DB through Sequelize.

--- util/database.js
+++ util/database.js
@@ -1,11 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize').Sequelize;
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces.env.DB_PASS, {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
