40058e717fba084b31f5f5e4aa2c21abd06ad57f
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-13 18:19:22 -0300

    Create and manage dummy user.

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const notFoundRoute = require('./routes/not-found');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(notFoundRoute);
@@ -29,10 +36,17 @@ Product.belongsTo(User, {
 
 User.hasMany(Product);
 
-sequelize.sync({
-    force: true
-}).then(result => {
-    // console.log(result);
+sequelize.sync().then(() => {
+    return User.findByPk(1);
+}).then(user => {
+    if (!user) {
+        return User.create({
+            name: 'Maximo',
+            email: 'test@test.com'
+        })
+    }
+    return Promise.resolve(user);
+}).then(() => {
     app.listen(3000);
 }).catch(err => {
     console.log(err);
