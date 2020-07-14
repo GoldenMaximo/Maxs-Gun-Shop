3befe5e3016a422aedeb4de8af0f1c2d47a67b08
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-14 18:36:00 -0300

    Create cart and cart-item model. Create one-to-many & many-to-many cart/product relations.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -33,8 +35,11 @@ Product.belongsTo(User, {
     constraints: true,
     onDelete: 'CASCADE'
 });
-
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize.sync().then(() => {
     return User.findByPk(1);
