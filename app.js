55e5cc92273481722320bc1e77cf5820fe14ecb2
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-15 11:55:41 -0300

    Add order model.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -40,6 +42,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
 
 sequelize.sync().then(() => {
     return User.findByPk(1);
