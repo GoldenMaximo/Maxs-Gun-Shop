a9e0f074a4a53359666a46ec36cc277840eb8508
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-22 20:45:25 -0300

    Fetch all products.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 // const notFoundRoute = require('./routes/not-found');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 // app.use(notFoundRoute);
 
 mongoConnect(() => {
--- models/Product.js
+++ models/Product.js
@@ -14,6 +14,13 @@ class Product {
             console.log(result);
         }).catch(err => console.log(err));
     }
+
+    static fetchAll() {
+        const db = getDb();
+        return db.collection('products').find().toArray().then(products => {
+            return products
+        }).catch(err => console.log(err));
+    }
 }
 
 module.exports = Product;
