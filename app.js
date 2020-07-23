ed7e3d7e17c6e5c7cd288ac69188dc8ec6166135
Gustavo Máximo <gfmaximo97@gmail.com>
2020-07-23 15:43:35 -0300

    Create user model for MongoDB.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoConnect } = require('./util/database');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,10 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1).then(user => {
-    //     req.user = user;
-    //     next();
-    // }).catch(err => console.log(err));
+    User.findById('5f19d95fd7f4d360ed400fdf').then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
     next();
 });
 
--- models/user.js
+++ models/user.js
@@ -1,22 +1,23 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const { getDb } = require('../util/database');
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
     }
-})
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({
+            _id: new mongodb.ObjectId(userId)
+        });
+    }
+}
 
 module.exports = User;
