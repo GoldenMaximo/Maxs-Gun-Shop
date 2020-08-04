d0690163aae97ca2e707d0c46590ddd9f6c3bd90
Gustavo Máximo <gfmaximo97@gmail.com>
2020-08-04 15:30:51 -0300

    Fetch all products (index/products page).

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,11 @@
-const path = require('path');
 const express = require('express');
-
 const router = express.Router();
+
 const shopController = require('../controllers/shop');
 
-// router.get('/', shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-// router.get('/products', shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
 // router.get('/products/:productId', shopController.getProduct);
 
