bcf13ff97d91d232c28259fb6dcde8fcd08de2ce
Gustavo Máximo <gfmaximo97@gmail.com>
2020-08-18 19:45:47 -0300

    Normalize user input on signup and login.

--- routes/auth.js
+++ routes/auth.js
@@ -11,32 +11,47 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 
 router.post('/login', [
-    body('email').isEmail().withMessage('Invalid email or password'),
-    body('password', 'Invalid email or password').isAlphanumeric().isLength({ min: 8 })
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email or password')
+        .normalizeEmail(),
+    body('password', 'Invalid email or password')
+        .isAlphanumeric()
+        .isLength({ min: 8 })
+        .trim()
 ], authController.postLogin);
 
 router.post(
     '/signup',
     [
-        check('email').isEmail().withMessage('Please enter a valid email').custom((value, { req }) => {
-            // If express validator indentifies a promise it waits until the promise is done
-            // If the promise returns something or nothing it passes the validation
-            // If the promise fails it fails the validation
-            return User.findOne({
-                email: value
-            }).then(user => {
-                if (user) {
-                    return Promise.reject('This email is already in use');
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .custom((value, { req }) => {
+                // If express validator indentifies a promise it waits until the promise is done
+                // If the promise returns something or nothing it passes the validation
+                // If the promise fails it fails the validation
+                return User.findOne({
+                    email: value
+                }).then(user => {
+                    if (user) {
+                        return Promise.reject('This email is already in use');
+                    }
+                });
+            })
+            .normalizeEmail(),
+        body('password', 'Please enter a password with only numbers and text and at least 8 characters')
+            .isLength({ min: 8 })
+            .isAlphanumeric()
+            .trim(),
+        body('confirmPassword')
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error('Passwords have to match!')
                 }
-            });
-        }),
-        body('password', 'Please enter a password with only numbers and text and at least 8 characters').isLength({ min: 8 }).isAlphanumeric(),
-        body('confirmPassword').custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('Passwords have to match!')
-            }
-            return true;
-        }),
+                return true;
+            }),
     ],
     authController.postSignup
 );
