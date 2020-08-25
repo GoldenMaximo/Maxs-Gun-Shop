a9ce3d48be3afd9c8e8e478c0f07c61dad204c2a
Gustavo Máximo <gfmaximo97@gmail.com>
2020-08-25 18:12:42 -0300

    Create pagination template. Add pagination to the products page.

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,14 +7,26 @@ const PDFDocument = require('pdfkit');
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-    Product.find().then(products => {
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find().countDocuments().then(numProducts => {
+        totalItems = numProducts;
+        return Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+    }).then(products => {
         res.render('shop/product-list', {
             prods: products,
             pageTitle: 'All Products',
             path: '/products',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
     }).catch(err =>
-        console.log(err)
+        next(err)
     );
 };
 
@@ -49,7 +61,7 @@ exports.getIndex = (req, res, next) => {
             lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
     }).catch(err => {
-        console.log(err);
+        next(err);
     });
 };
 
